335e9fc56ac0f531a79f17920439bace44e2eec8
Krati Paw <39027292+kratipaw@users.noreply.github.com>
2019-11-18 15:39:12 +0530

    Update WordLadder1.java

--- L8_Graph/WordLadder1.java
+++ L8_Graph/WordLadder1.java
@@ -46,7 +46,7 @@ public class WordLadder1 {
 		System.out.println(new WordLadder1().ladderLength(start, end, dictV));
 	}
 
-	public int ladderLength(String start, String end, ArrayList<String> dictV) {
+	public int ladderLength_tle(String start, String end, ArrayList<String> dictV) {
     
 		Queue<String> qu = new LinkedList<String>();
         
@@ -90,4 +90,109 @@ public class WordLadder1 {
         
         return 0;
 	}
+	
+	class Pair {
+        
+        String key;
+        int value;
+        
+        public Pair(String s, int v){
+            key = s;
+            value = v;
+        }
+        
+        public String getKey() {
+            return key;
+        }
+        
+        public int getValue() {
+            return value;
+        }
+    }
+    
+    public int solve(String A, String B, ArrayList<String> C) {
+        
+        Set<String> dict = new HashSet<>(C);
+        Set<String> start = new HashSet<>();
+        start.add(A);
+        dict.remove(A);
+        
+        Map<String, ArrayList<String>> map = new HashMap<>();
+        
+        while(!start.isEmpty()) {
+            if (start.contains(B)) {
+                break;
+            }
+            
+            Set<String> set = new HashSet<>();
+            for (String word : start) {
+                ArrayList<String> relatedWords = getRelatedWords(word, dict);
+                if (!relatedWords.isEmpty()) {
+                    map.put(word, relatedWords);
+                    set.addAll(relatedWords);
+                }
+            }
+            
+            dict.removeAll(set);
+            start = set;
+        }
+        
+        Queue<Pair> qu = new LinkedList<Pair>();
+        
+        qu.add(new Pair(A, 1));
+        
+        HashMap<String, Boolean> visited = new HashMap<String, Boolean>();
+        visited.put(A, true);
+        
+        while(!qu.isEmpty()){
+         
+            Pair node = qu.poll();
+            
+            String curr = node.getKey();
+            int level = node.getValue();
+            
+            ArrayList<String> relatedWords = map.getOrDefault(curr, new ArrayList<String>());
+                
+            for(String relatedWord : relatedWords){
+                if(relatedWord.equalsIgnoreCase(B)){
+                    return level + 1;
+                }
+
+                if(!visited.containsKey(relatedWord)){
+                    visited.put(relatedWord, true);
+                    Pair newEntry = new Pair(relatedWord, level + 1);
+                    qu.add(newEntry);
+                }
+            }
+        }
+        
+        return 0;
+    }
+    
+    public ArrayList<String> getRelatedWords(String word, Set<String> dict) {
+        
+        ArrayList<String> relatedWords = new ArrayList<String>();
+        
+        char[] wordArr = word.toCharArray();
+        
+        for(int i = 0; i < wordArr.length; i++){
+            
+            char ch = word.charAt(i);
+            
+            for(char c = 'a'; c <= 'z'; c++){
+                
+                if(c == ch)
+                    continue;
+                
+                wordArr[i] = c;
+                
+                String newWord = String.valueOf(wordArr);
+                if(dict.contains(newWord))
+                    relatedWords.add(newWord);
+            }
+            wordArr[i] = ch;
+        }
+        
+        return relatedWords;
+    }
 }
